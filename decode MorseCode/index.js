364485d9eeeb93dcfeeef888e7c93f056461500b
Tatyana Dotsenko <tetiana_dotsenko@epam.com>
2020-10-22 14:28:05 +0300

    task solved

---
+++ decode MorseCode/index.js
@@ -0,0 +1,71 @@
+// The task is to implement a function that would take the morse code as input
+// and return a decoded human-readable string.
+//
+// Example:
+// decodeMorse('.... . -.--   . . . ...- .-')
+// should return "hey eva"
+
+let MORSE_CODE = {
+  "-----":"0",
+  ".----":"1",
+  "..---":"2",
+  "...--":"3",
+  "....-":"4",
+  ".....":"5",
+  "-....":"6",
+  "--...":"7",
+  "---..":"8",
+  "----.":"9",
+  ".-":"a",
+  "-...":"b",
+  "-.-.":"c",
+  "-..":"d",
+  ".":"e",
+  "..-.":"f",
+  "--.":"g",
+  "....":"h",
+  "..":"i",
+  ".---":"j",
+  "-.-":"k",
+  ".-..":"l",
+  "--":"m",
+  "-.":"n",
+  "---":"o",
+  ".--.":"p",
+  "--.-":"q",
+  ".-.":"r",
+  "...":"s",
+  "-":"t",
+  "..-":"u",
+  "...-":"v",
+  ".--":"w",
+  "-..-":"x",
+  "-.--":"y",
+  "--..":"z",
+  "/":" ",
+  "-·-·--":"!",
+  "·-·-·-":".",
+  "--··--":","
+};
+
+decodeMorse = function(morseCode){
+  let words = morseCode.split("   ");
+  let string = ""
+
+  for (let i in words){
+    if(words[i] != ''){
+      let word = words[i].split(" ");
+      for(let j in word){
+        if(word[j] != ''){
+          string += MORSE_CODE[word[j]];
+        }
+      }
+      if(i < words.length-1){
+        string += " "
+      }
+    }
+  }
+  return string
+}
+
+decodeMorse('.... . -.--   . ...- .-')
